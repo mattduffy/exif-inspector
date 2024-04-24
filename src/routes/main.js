b7076cdec6cd31f5fa2d461402dea3f204575bdc
Matthew Duffy <mattduffy@gmail.com>
2024-04-24 13:28:24 -0700

    Add -quiet option to the exiftool raw query for listing inpsected files.  Not sure why error reporting behavior changed, but updated system exiftool version to 12.84 as well.

--- src/routes/main.js
+++ src/routes/main.js
@@ -557,8 +557,11 @@ router.get('listUploadedImages', '/x', async (ctx) => {
   let tool
   try {
     tool = new Exiftool()
-    images = await tool.raw('/usr/local/bin/exiftool -json --ext md -groupNames -b -dateFormat %s -File:Filename -File:MIMEType -FileModifyDate -ThumbnailImage -PreviewPNG -PreviewImage ./inspected')
-    images.sort((a, b) => b['File:FileModifyDate'] - a['File:FileModifyDate'])
+    images = await tool.raw('/usr/local/bin/exiftool -quiet -json --ext md -groupNames -b -dateFormat %s -File:Filename -File:MIMEType -FileModifyDate -ThumbnailImage -PreviewPNG -PreviewImage ./inspected')
+    log('images: ', images)
+    if (images.length > 0) {
+      images.sort((a, b) => b['File:FileModifyDate'] - a['File:FileModifyDate'])
+    }
   } catch (e) {
     error('Failed to exiftool inspected images.')
     error(e)
