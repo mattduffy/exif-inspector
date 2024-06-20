6a621b497dfb542101567239cfc89c78b7e7a174
Matthew Duffy <mattduffy@gmail.com>
2024-06-20 16:36:41 -0700

    Update the loginAttempts middleware to run on both un/ and successful login attempts.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -92,11 +92,12 @@ router.post('postLogin', '/login', async (ctx) => {
     const users = new Users(collection, ctx)
     const authUser = await users.authenticateAndGetUser(username, password)
     log('authentication result: %o', authUser)
+    const doc = { attemptedAt: new Date(), username }
+    if (ctx.state?.logEntry) {
+      doc.from = { ip: ctx.state.logEntry.ip, geo: ctx.state.logEntry.geo }
+    }
     if (!authUser.user) {
-      const doc = { attemptedAt: new Date(), username, password }
-      if (ctx.state?.logEntry) {
-        doc.from = { ip: ctx.state.logEntry.ip, geo: ctx.state.logEntry.geo }
-      }
+      doc.incorrectPassword = password
       await db.collection('loginAttempts').insertOne(doc)
       ctx.state.isAuthenticated = false
       ctx.flash = {
@@ -109,6 +110,7 @@ router.post('postLogin', '/login', async (ctx) => {
       }
       ctx.redirect('/login')
     } else if (authUser) {
+      await db.collection('loginAttempts').insertOne(doc)
       log('successful user login')
       authUser.user.sessionId = sessionId
       const loggedInUser = await authUser.user.update()
