1cf396ebb9e7eee71b0a6c67e992281df5997bd4
Matthew Duffy <mattduffy@gmail.com>
2025-09-16 21:28:18 -0700

    Update the deleteData.js script based on improvements made in koa-glp.

--- src/utils/deleteData.js
+++ src/utils/deleteData.js
@@ -9,7 +9,7 @@ import path from 'node:path'
 import * as dotenv from 'dotenv'
 import { fileURLToPath } from 'node:url'
 import { Command } from 'commander'
-import { redis } from '../daos/impl/redis/redis-client.js'
+import { redis_single as redis } from '../daos/impl/redis/redis-single.js'
 import { _log, _error } from './logging.js'
 /* eslint-enable import/no-extraneous-dependencies */
 
@@ -33,79 +33,68 @@ dotenv.config({
 const DB_PREFIX = redisEnv.REDIS_KEY_PREFIX
 
 const program = new Command()
-program.name('loadData')
-  .option('--key-count <count>', 'The number of keys to return per cursor.', 500)
-  .requiredOption('--key-type <type>', 'The redis data type of the keys to delete.')
+program.name('deleteData')
   .requiredOption('--key-prefix <prefix>', 'The app-specific key prefix for Redis to use.')
-  .requiredOption(
-    '--key-name <name>',
-    'The key name for Redis to append to the app-specific key prefix.',
-  )
+  .requiredOption('--key-type <type>', 'The redis data type of the keys to delete.')
+  .requiredOption('--key-name <name>', 'Key name to append to the app-specific key prefix.')
+  .option('--key-count <count>', 'The number of keys to return per cursor.', 20)
+  .option('--dry-run', 'Dry run the delete command, don\'t actully delete anything.')
 
 program.parse(process.argv)
 const options = program.opts()
 options.dbPrefix = DB_PREFIX
-log(options)
+log('options:', options)
 
-const transparentKeyPrefix = redis?.options?.keyPrefix
-let keyPath
-if (transparentKeyPrefix === null
-  || transparentKeyPrefix === undefined
-  || transparentKeyPrefix === '') {
-  keyPath = `${DB_PREFIX}:${options.keyPrefix}:${options.keyName}:*`
-} else {
-  keyPath = `${transparentKeyPrefix}${options.keyName}:*`
-}
-log(`prefix: ${keyPath}`)
-// log(`redis.optins.keyPrefix: ${redis.options.keyPrefix}`)
+let keyPath = options?.keyPrefix ?? options.dbPrefix
+log(`full keyPath: ${keyPath}:${options.keyName}`)
+log(`redis.options.keyPrefix: ${redis.options.keyPrefix}`)
 // process.exit()
 
 async function del() {
-  return new Promise((resolve, reject) => {
-    let deletedKeys = 0
-    const scanArgs = {
-      match: keyPath,
-      type: options.keyType,
-      count: options.keyCount,
+  const scanArgs = {
+    CURSOR: '0',
+    MATCH: `${keyPath}:${options.keyName}`,
+    TYPE: options.keyType,
+    COUNT: options.keyCount,
+  }
+  log(scanArgs)
+  let myIterator = await redis.scanIterator(scanArgs)
+  let batch
+  let count = 0
+  while (batch = await myIterator.next()) {
+    if (batch.done) {
+      break
     }
-    log(scanArgs)
-    const stream = redis.scanStream(scanArgs)
-    stream.on('data', async (keys) => {
-      log(`Current scan cursor size: ${keys.length}`)
-      let result
-      if (keys.length > 0) {
-        stream.pause()
-        const pipeline = redis.pipeline()
-        keys.forEach(async (key) => {
-          log(`current cursor key: ${key}`)
-          // Super sketchy hack to get around ioredis client config with transparent
-          // key prefix set.
-          // May be super fragile...
-          const k = key.split(':').slice(-1)[0]
-          pipeline.del(`${options.keyName}:${k}`)
-          deletedKeys += 1
-        })
-        result = await pipeline.exec()
-        log(`pipeline result: ${result}`)
-        stream.resume()
+    for await (const k of batch.value) {
+      let deleted
+      if (options.keyType === 'ReJSON-RL' ) {
+        if (!options.dryRun) {
+          deleted = await redis.json.del(k) 
+        } else {
+          log(`DRY-RUN: redis.json.del(${k})`)
+        }
       } else {
-        log(`no keys found matching ${keyPath}`)
+        if (!options.dryRun) {
+          deleted = await redis.del(k)
+        } else {
+          log(`DRY-RUN: redis.del(${k})`)
+        }
       }
-    })
-    stream.on('end', () => {
-      resolve(`Deleted ${deletedKeys} from ${keyPath}`)
-    })
-    stream.on('error', (e) => {
-      reject(e)
-    })
-  })
+      console.log('deleted', k, deleted)
+      count += 1
+    }
+  }
+  return count
 }
 try {
   const result = await del()
-  log(result)
+  log(`key ${keyPath}:${options.keyName} deleted?`, result)
 } catch (e) {
   error(e)
-  throw new Error(e.message, { cause: e })
+  // throw new Error(e.message, { cause: e })
+  error(e.message)
+  info('Try overriding the default redis user/password with ones that can use DEL.')
+  info('R_PRIV_USER=<user> R_PRIV_PASSWORD=<psswd> npm run deleteData ...')
 }
 
 // Done deleting the data, exit process.
