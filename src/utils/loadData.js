b39da4f4beb0b7e324f5b4728e45183f9dc99b4c
Matthew Duffy <mattduffy@gmail.com>
2026-09-19 10:24:47 -0700

    Remove lingering refereneces to redis-om, even if they are commented out.

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -537,7 +537,7 @@ try {
       log(' ')
 
       if (!DRYRUN || SAVEFILES) { // DRYRUN CHECK
-        // save the pier with redis-om repository
+        // save the pier with redis om repository
         const saved = await pierRepository.save(pierJson.pier, pierJson)
         log(saved)
       }
